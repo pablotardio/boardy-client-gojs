8d9b85ede39afd899fd69d5c2935010611f67b84
pablotardio <pablo.tv30@gmail.com>
2021-02-26 18:15:25 -0400

    fixed url double https

--- src/providers/url.provider.js
+++ src/providers/url.provider.js
@@ -1,4 +1,4 @@
 //const urlProvider="http://192.168.1.2:3002/api"
 
-const urlProvider="https://https://boardy-server.herokuapp.com/api"
+const urlProvider="https://boardy-server.herokuapp.com/api"
 export default urlProvider

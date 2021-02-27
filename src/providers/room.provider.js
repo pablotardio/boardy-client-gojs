a1d736d5d4774fe68aca8093850f798ab50ab1ad
pablotardio <pablo.tv30@gmail.com>
2021-02-27 00:41:21 -0400

    API and Socket Urls  ARE different, 9 hours because of that and db error

--- src/providers/room.provider.js
+++ src/providers/room.provider.js
@@ -1,6 +1,6 @@
-import urlProvider from "./url.provider";
+import APIurlProvider from "./url.provider";
 
-const url = urlProvider;
+const url = APIurlProvider;
 class RoomProvider {
      
     static async verifySave(body) {
--- src/providers/url.provider.js
+++ src/providers/url.provider.js
@@ -1,4 +1,5 @@
-//const urlProvider="http://192.168.1.2:3002/api"
+const urlProvider="https://boardy-server.herokuapp.com"
+const APIurlProvider=urlProvider+"/api"
 
-const urlProvider="https://boardy-server.herokuapp.com/api"
-export default urlProvider
+//const urlProvider="https://boardy-server.herokuapp.com/api"
+export default APIurlProvider

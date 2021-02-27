e2a6b122a11f056e7907968d3b0d78e72603e25e
pablotardio <pablo.tv30@gmail.com>
2021-02-27 16:24:11 -0400

    redirecting correct to register from login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,7 +5,7 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
+
 import Paper from "@material-ui/core/Paper";
 import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
@@ -13,7 +13,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import AuthProvider from "../providers/auth.provider";
-import { useHistory } from "react-router-dom";
+import { useHistory,Link } from "react-router-dom";
 
 function Copyright() {
 	return (
@@ -104,7 +104,7 @@ const  LoginPage = ({updateNav}) => {
 						Boardy
 					</Typography>
 					<Typography component="h1" variant="h5">
-						Sign in
+						Iniciar sesion
 					</Typography>
 					<form
 						className={classes.form}
@@ -137,10 +137,10 @@ const  LoginPage = ({updateNav}) => {
 							id="password"
 							autoComplete="current-password"
 						/>
-						<FormControlLabel
+						{/* <FormControlLabel
 							control={<Checkbox color="primary" />}
 							label="Remember me"
-						/>
+						/> */}
 						<Button
 							type="submit"
 							fullWidth
@@ -148,17 +148,17 @@ const  LoginPage = ({updateNav}) => {
 							color="primary"
 							className={classes.submit}
 						>
-							Sign In
+							Login
 						</Button>
 						<Grid container>
 							<Grid item xs>
-								<Link href="#" variant="body2">
+								{/* <Link href="#" variant="body2">
 									Forgot password?
-								</Link>
+								</Link> */}
 							</Grid>
 							<Grid item>
-								<Link href="#" variant="body2">
-									{"Don't have an account? Sign Up"}
+								<Link to="/register" variant="body2">
+									{"No tienes una cuenta? Registrate"}
 								</Link>
 							</Grid>
 						</Grid>
